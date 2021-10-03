878f23785025d57741f436c0898d05a82eb9bd9c
Yo Kobayashi <y.kobayashi@4nonome.com>
2021-10-03 21:30:21 +0900

    create uitrace20210930

---
+++ uitrace/uitrace20210930/android/app/src/main/kotlin/com/example/uitrace20210930/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.uitrace20210930
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ uitrace/uitrace20210930/lib/main.dart
@@ -0,0 +1,9 @@
+import 'dart:ui';
+import 'package:flutter/material.dart';
+import 'package:uitrace20210930/bottom_navigation.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: BottomNavigation(),
+  ));
+}
