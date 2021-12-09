038fc3b01c7f7df8d0d9f20d6611fe6fd7d65175
Yo <82624334+KobayashiYoh@users.noreply.github.com>
2021-12-09 17:19:34 +0900

    Delete mobile_qiita_app directory
    mainブランチを荒らしてしまったので、Flutterプロジェクトを削除して作り直します。

--- mobile_qiita_app/android/app/src/main/kotlin/com/example/mobile_qiita_app/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.mobile_qiita_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- mobile_qiita_app/lib/main.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:mobile_qiita_app/pages/top_page.dart';
-
-void main() => runApp(MaterialApp(
-  initialRoute: '/',
-  routes: {
-    '/': (context) => TopPage(),
-  },
-));
