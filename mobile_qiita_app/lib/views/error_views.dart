da5103b574b5628c969f1ac50724011a65bb609b
Yo Kobayashi <y.kobayashi@4nonome.com>
2022-02-09 11:34:35 +0900

    create not login UI for My Page

--- mobile_qiita_app/lib/views/error_views.dart
+++ mobile_qiita_app/lib/views/error_views.dart
@@ -86,4 +86,60 @@ class ErrorView {
       ),
     );
   }
+
+  // 未ログインの場合に表示
+  static Widget notLoginView(/* Function onTapLogin */) {
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          padding: const EdgeInsets.symmetric(vertical: 40.0, horizontal: 16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Container(),
+              Container(
+                child: Column(
+                  children: <Widget>[
+                    Container(
+                      padding: EdgeInsets.only(top: 40.0, bottom: 8.0),
+                      child: const Text(
+                        'ログインが必要です',
+                      ),
+                    ),
+                    const Text(
+                      'マイページの機能を利用するには\nログインを行っていただく必要があります。',
+                      style: TextStyle(
+                        color: Colors.grey,
+                        fontSize: 12.0,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                  ],
+                ),
+              ),
+              FlatButton(
+                onPressed: () {
+                  // onTapLogin();
+                },
+                height: 48.0,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(24.0),
+                  ),
+                ),
+                child: const Text(
+                  'ログインする',
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
+                ),
+                color: Colors.green,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
 }
