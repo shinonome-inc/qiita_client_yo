6ce9d5d52bda0d94ffeed4ef16dc8293f863e304
Yo Kobayashi <y.kobayashi@4nonome.com>
2022-02-12 21:20:15 +0900

    update userFormat UI

--- mobile_qiita_app/lib/widgets/widget_formats.dart
+++ mobile_qiita_app/lib/widgets/widget_formats.dart
@@ -1,4 +1,5 @@
 import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:mobile_qiita_app/common/constants.dart';
 import 'package:mobile_qiita_app/common/methods.dart';
@@ -117,16 +118,64 @@ class WidgetFormats {
     String userIconUrl =
         user.iconUrl.isNotEmpty ? user.iconUrl : Constants.defaultUserIconUrl;
     return Container(
+      padding: const EdgeInsets.symmetric(vertical: 16.0, horizontal: 24.0),
       child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
-          CircleAvatar(
-            radius: 24.0,
-            backgroundImage: CachedNetworkImageProvider(userIconUrl),
+          Container(
+            alignment: Alignment.centerLeft,
+            padding: const EdgeInsets.only(bottom: 16.0),
+            child: CircleAvatar(
+              radius: 24.0,
+              backgroundImage: CachedNetworkImageProvider(userIconUrl),
+            ),
           ),
           Text(user.name),
-          Text(user.id),
-          Text(user.description),
-          Text('${user.followingsCount}フォロー中 ${user.followersCount}フォロワー'),
+          Text(
+            '@${user.id}',
+            style: TextStyle(
+              color: Colors.grey,
+            ),
+          ),
+          Container(
+            padding: const EdgeInsets.symmetric(vertical: 16.0),
+            child: Text(
+              '${user.description}${user.description}${user.description}${user.description}${user.description}',
+              maxLines: 3,
+              overflow: TextOverflow.ellipsis,
+              style: TextStyle(
+                color: Colors.grey,
+              ),
+            ),
+          ),
+          RichText(
+            text: TextSpan(style: TextStyle(color: Colors.black), children: [
+              TextSpan(
+                text: '${user.followingsCount}',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              TextSpan(
+                text: 'フォロー中  ',
+                style: TextStyle(
+                  color: Colors.grey,
+                ),
+              ),
+              TextSpan(
+                text: '${user.followersCount}',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              TextSpan(
+                text: 'フォロワー',
+                style: TextStyle(
+                  color: Colors.grey,
+                ),
+              ),
+            ]),
+          ),
         ],
       ),
     );
