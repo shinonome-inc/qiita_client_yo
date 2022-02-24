7f303f64ca4f136830f8058fb6fe07a4d3d3db10
Yo Kobayashi <y.kobayashi@4nonome.com>
2022-02-24 23:32:59 +0900

    fix Incorrect use of ParentDataWidget.

--- mobile_qiita_app/lib/components/user_component_of_user_list.dart
+++ mobile_qiita_app/lib/components/user_component_of_user_list.dart
@@ -8,98 +8,86 @@ class UserComponentOfUserList extends StatelessWidget {
       : super(key: key);
   final User user;
 
-  // FIXME: 実行はできるが、"Incorrect use of ParentDataWidget."というエラーが発生
   @override
   Widget build(BuildContext context) {
-    return Flexible(
-      child: GestureDetector(
-        onTap: () {
-          // TODO: UserPageへ遷移
-        },
+    return GestureDetector(
+      onTap: () {
+        // TODO: UserPageへ遷移
+      },
+      child: Container(
+        margin: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(8.0),
+          border: Border.all(
+            color: const Color(0xFFE0E0E0),
+            width: 1.0,
+          ),
+        ),
         child: Container(
           margin: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
-          decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.circular(8.0),
-            border: Border.all(
-              color: const Color(0xFFE0E0E0),
-              width: 1.0,
-            ),
-          ),
-          child: Container(
-            color: Colors.white,
-            margin: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Container(
-                  height: 32.0,
-                  child: Row(
-                    children: <Widget>[
-                      CircleAvatar(
-                        radius: 16,
-                        backgroundImage:
-                            CachedNetworkImageProvider(user.iconUrl),
-                      ),
-                      Flexible(
-                        child: Container(
-                          height: 32.0,
-                          margin: const EdgeInsets.only(left: 8.0),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: <Widget>[
-                              Container(
-                                height: 20.0,
-                                child: Text(
-                                  // user.name,
-                                  'なまええええええええええええええええええええええええ',
-                                  overflow: TextOverflow.ellipsis,
-                                  maxLines: 1,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Container(
+                child: Row(
+                  children: <Widget>[
+                    CircleAvatar(
+                      radius: 16,
+                      backgroundImage: CachedNetworkImageProvider(user.iconUrl),
+                    ),
+                    Flexible(
+                      child: Container(
+                        margin: const EdgeInsets.only(left: 8.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Container(
+                              height: 20.0,
+                              child: Text(
+                                user.name,
+                                overflow: TextOverflow.ellipsis,
+                                maxLines: 1,
+                              ),
+                            ),
+                            Container(
+                              height: 12.0,
+                              child: Text(
+                                '@${user.id}',
+                                overflow: TextOverflow.ellipsis,
+                                maxLines: 1,
+                                style: TextStyle(
+                                  color: const Color(0xFF828282),
+                                  fontSize: 12.0,
                                 ),
                               ),
-                              Container(
-                                height: 12.0,
-                                child: Text(
-                                  // '@${user.id}',
-                                  'iddddddddddddddddddddddddddddddddddddddddddddddd',
-                                  overflow: TextOverflow.ellipsis,
-                                  maxLines: 1,
-                                  style: TextStyle(
-                                    color: const Color(0xFF828282),
-                                    fontSize: 12.0,
-                                  ),
-                                ),
-                              ),
-                            ],
-                          ),
+                            ),
+                          ],
                         ),
                       ),
-                    ],
-                  ),
+                    ),
+                  ],
                 ),
-                Container(
-                  height: 12.0,
-                  margin: const EdgeInsets.symmetric(vertical: 8.0),
-                  child: Text(
-                    // 'Posts: ${user.posts}',
-                    'Posts: 999999999999999999999999999999999999999999999999',
-                    overflow: TextOverflow.ellipsis,
-                    maxLines: 1,
-                    style: TextStyle(fontSize: 12.0),
-                  ),
+              ),
+              Container(
+                height: 12.0,
+                margin: const EdgeInsets.symmetric(vertical: 8.0),
+                child: Text(
+                  'Posts: ${user.posts}',
+                  overflow: TextOverflow.ellipsis,
+                  maxLines: 1,
+                  style: TextStyle(fontSize: 12.0),
                 ),
-                Container(
-                  child: Text(
-                    //user.description,
-                    '3行を超える長い自己紹介3行を超える長い自己紹介3行を超える長い自己紹介3行を超える長い自己紹介3行を超える長い自己紹介3行を超える長い自己紹介3行を超える長い自己紹介3行を超える長い自己紹介3行を超える長い自己紹介3行を超える長い自己紹介3行を超える長い自己紹介',
-                    overflow: TextOverflow.ellipsis,
-                    maxLines: 3,
-                    style: TextStyle(
-                        color: const Color(0xFF828282), fontSize: 12.0),
-                  ),
+              ),
+              Container(
+                child: Text(
+                  user.description,
+                  overflow: TextOverflow.ellipsis,
+                  maxLines: 3,
+                  style:
+                      TextStyle(color: const Color(0xFF828282), fontSize: 12.0),
                 ),
-              ],
-            ),
+              ),
+            ],
           ),
         ),
       ),
