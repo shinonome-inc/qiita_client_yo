b5caecf70df486bb86374e3a8cb6b20e89f8a96c
Yo Kobayashi <y.kobayashi@4nonome.com>
2022-03-17 10:26:59 +0900

    fix layout collapse for EmptySearchResultView class

--- mobile_qiita_app/lib/views/empty_serch_result_view.dart
+++ mobile_qiita_app/lib/views/empty_serch_result_view.dart
@@ -7,23 +7,23 @@ class EmptySearchResultView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Container(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('検索にマッチする記事はありませんでした'),
-            const SizedBox(height: 20.0),
-            const Text(
-              '検索条件を変えるなどして再度検索をしてください',
-              style: TextStyle(
-                color: Constants.lightSecondaryGrey,
-                fontSize: 12.0,
-              ),
+    double deviceHeight = MediaQuery.of(context).size.height;
+    return Container(
+      margin: EdgeInsets.only(top: deviceHeight / 2 - 200),
+      height: 61.0,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          const Text('検索にマッチする記事はありませんでした'),
+          const SizedBox(height: 20.0),
+          const Text(
+            '検索条件を変えるなどして再度検索をしてください',
+            style: TextStyle(
+              color: Constants.lightSecondaryGrey,
+              fontSize: 12.0,
             ),
-            const SizedBox(height: 78.0), // 画面比率の調整
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
