b1b3de8e7c4eccb6c0c9b2a56241c481f5825474
Yo Kobayashi <y.kobayashi@4nonome.com>
2022-03-17 18:19:47 +0900

    remake SearcableAppBar class into _searchableAppBar method and refresh TextField when search result is zero

--- mobile_qiita_app/lib/pages/feed_page.dart
+++ mobile_qiita_app/lib/pages/feed_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:mobile_qiita_app/common/constants.dart';
 import 'package:mobile_qiita_app/components/list_components/article_list_view.dart';
-import 'package:mobile_qiita_app/components/searchable_app_bar_component.dart';
 import 'package:mobile_qiita_app/extension/pagination_scroll.dart';
 import 'package:mobile_qiita_app/models/article.dart';
 import 'package:mobile_qiita_app/services/qiita_client.dart';
@@ -17,6 +17,8 @@ class FeedPage extends StatefulWidget {
 
 class _FeedPageState extends State<FeedPage> {
   final ScrollController _scrollController = ScrollController();
+  final TextEditingController _textEditingController = TextEditingController();
+  Size get preferredSize => Size.fromHeight(kToolbarHeight + 46.0);
   late Future<List<Article>> _futureArticles;
   List<Article> _fetchedArticles = [];
 
@@ -27,6 +29,65 @@ class _FeedPageState extends State<FeedPage> {
 
   bool _isNetworkError = false;
   bool _isLoading = false;
+  bool _isEmptySearchResult = false;
+
+  PreferredSize? _searchableAppBar() {
+    return PreferredSize(
+      preferredSize: preferredSize,
+      child: Container(
+        decoration: BoxDecoration(
+          border: Border(
+            bottom: BorderSide(
+              color: const Color(0xFFCFCFCF),
+              width: 0.2,
+            ),
+          ),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: <Widget>[
+            Flexible(
+              child: Container(
+                child: Center(
+                  child: Text(
+                    'Feed',
+                    style: Constants.appBarTextStyle,
+                  ),
+                ),
+              ),
+            ),
+            Container(
+              height: 36.0,
+              margin:
+                  const EdgeInsets.only(left: 16.0, right: 16.0, bottom: 8.0),
+              padding: const EdgeInsets.only(left: 8.0),
+              decoration: BoxDecoration(
+                color: const Color(0xEFEFF0FF),
+                borderRadius: BorderRadius.circular(10.0),
+              ),
+              child: TextField(
+                enabled: true,
+                decoration: InputDecoration(
+                  border: InputBorder.none,
+                  icon: const Icon(
+                    Icons.search,
+                    color: Color(0xFF8E8E93),
+                  ),
+                  hintText: 'Search',
+                  hintStyle: TextStyle(
+                    color: const Color(0xFF8E8E93),
+                    fontSize: 17.0,
+                  ),
+                ),
+                controller: _textEditingController,
+                onSubmitted: _searchForArticlesFromInputText,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 
   void _searchForArticlesFromInputText(String inputText) {
     _searchWord = inputText;
@@ -37,6 +98,13 @@ class _FeedPageState extends State<FeedPage> {
   }
 
   Future<void> _reload() async {
+    if (_isEmptySearchResult) {
+      setState(() {
+        _isEmptySearchResult = false;
+        _searchWord = '';
+        _textEditingController.text = '';
+      });
+    }
     _currentPageNumber = 1;
     _fetchedArticles.clear();
     setState(() {
@@ -81,10 +149,7 @@ class _FeedPageState extends State<FeedPage> {
         FocusScope.of(context).unfocus();
       },
       child: Scaffold(
-        appBar: SearchableAppBarComponent(
-          title: 'Feed',
-          searchArticles: _searchForArticlesFromInputText,
-        ),
+        appBar: _searchableAppBar(),
         body: RefreshIndicator(
           onRefresh: _reload,
           child: FutureBuilder(
@@ -100,7 +165,7 @@ class _FeedPageState extends State<FeedPage> {
                   snapshot.connectionState == ConnectionState.done;
               bool isWaiting = (_isNetworkError || _currentPageNumber == 1) &&
                   snapshot.connectionState == ConnectionState.waiting;
-              bool isEmptySearchResult = hasData && snapshot.data.length == 0;
+              _isEmptySearchResult = hasData && snapshot.data.length == 0;
 
               if (isInitialized) {
                 child = ArticleListView(
@@ -110,7 +175,7 @@ class _FeedPageState extends State<FeedPage> {
                 );
               }
 
-              if (isEmptySearchResult) {
+              if (_isEmptySearchResult) {
                 _isLoading = false;
                 _isNetworkError = false;
                 child = EmptySearchResultView();
