9578c2ed11c48abd0d1e3061801cd4e802b32369
KobayashiYoh <y.kobayashi@4nonome.com>
2023-01-08 19:28:28 +0900

    不要になったクラスを削除

--- lib/common/app_shape.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:mobile_qiita_app/common/app_border_radius.dart';
-
-class AppShape {
-  static const ShapeBorder modalHeaderShape = RoundedRectangleBorder(
-    borderRadius: AppBorderRadius.modalHeaderBorderRadius,
-  );
-}
