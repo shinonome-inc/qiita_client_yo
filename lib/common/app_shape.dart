1de0cd682f8470dafeab8e17e73ed1faaa159839
KobayashiYoh <y.kobayashi@4nonome.com>
2023-01-08 19:13:54 +0900

    モーダルの角丸を表現するためのクラスを作成

---
+++ lib/common/app_shape.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:mobile_qiita_app/common/app_border_radius.dart';
+
+class AppShape {
+  static const ShapeBorder modalHeaderShape = RoundedRectangleBorder(
+    borderRadius: AppBorderRadius.modalHeaderBorderRadius,
+  );
+}
